1a0b5a69fae50c1c86a34a77e9496f7526ea3ca6
Jimmy Pan <jimmypan13570@gmail.com>
2024-11-19 01:09:16 -0800

    Redirect all to /

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,10 +1,11 @@
-import { Route } from "@solidjs/router";
+import { Navigate, Route } from "@solidjs/router";
 import BSPage from "./pages/BSPage";
 
 export default function Root() {
   return (
     <>
       <Route path="/" component={BSPage} />
+      <Route path="/*" component={() => <Navigate href={"/"} />} />
     </>
   );
 }
