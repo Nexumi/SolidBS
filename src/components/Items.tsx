eadb26a97838dc2f151fbb874ec29bb0e437889c
Jimmy Pan <jimmypan13570@gmail.com>
2024-11-19 00:05:01 -0800

    Replace ItemRow with ItemCard

--- src/components/Items.tsx
+++ src/components/Items.tsx
@@ -1,22 +1,23 @@
-import { Accessor, For } from "solid-js";
-import ItemRow from "./ItemRow";
+import { Accessor, For, Setter } from "solid-js";
+import ItemCard from "./ItemCard";
 import { Flex } from "./ui/flex";
 
-export default function Items(props: { participants: Accessor<string[]> }) {
-  const columnTitle = "w-full m-1 text-2xl text-center text-white";
-
+export default function Items(props: {
+  participants: Accessor<string[]>;
+  setParticipants: Setter<string[]>;
+}) {
   return (
     <>
-      <div class="m-0">
-        <Flex>
-          <p class={columnTitle}>Name</p>
-          <p class={columnTitle}>Price</p>
-          <p class={columnTitle}>Item</p>
-        </Flex>
+      <Flex justifyContent="start" class="space-x-2">
         <For each={[...Array(2)]}>
-          {() => <ItemRow participants={props.participants} />}
+          {() => (
+            <ItemCard
+              participants={props.participants}
+              setParticipants={props.setParticipants}
+            />
+          )}
         </For>
-      </div>
+      </Flex>
     </>
   );
 }
