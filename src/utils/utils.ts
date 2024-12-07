f9b6a890f200d85b4dd295c7c785b3ef229275d8
Jimmy Pan <jimmypan13570@gmail.com>
2024-12-06 19:19:37 -0800

    implement basic parser

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,5 +1,29 @@
-import { Item } from "./types";
+import { Item, ParamType } from "./types";
 
-export function parseURL(url: string): Item[] {
-  return [{ name: "", price: "", participants: [] }];
+export function parseURL(url: string = window.location.href) {
+  const result: Item[] = [];
+
+  const urlParams = new URLSearchParams(new URL(url).searchParams);
+
+  const names = eval(urlParams.get(ParamType.NAMES) || "[]");
+  const prices = eval(urlParams.get(ParamType.PRICES) || "[]");
+  const items = eval(urlParams.get(ParamType.ITEMS) || "[]");
+
+  const maxLength = Math.max(names.length, prices.length, items.length);
+
+  for (let i = 0; i < maxLength; i++) {
+    result.push({
+      name: items[i],
+      price: prices[i],
+      participants: names[i].split(", "),
+    });
+  }
+
+  result.push({
+    name: "",
+    price: "",
+    participants: [],
+  });
+
+  return result;
 }
