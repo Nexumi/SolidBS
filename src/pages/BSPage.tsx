1fcb7415ab363743267ccd631ee49cfb2144a7c2
Jimmy Pan <jimmypan13570@gmail.com>
2024-12-06 17:44:51 -0800

    connect items to loading

--- src/pages/BSPage.tsx
+++ src/pages/BSPage.tsx
@@ -37,7 +37,7 @@ export default function BSPage() {
             setItems(records);
           }}
         />
-        <LoadButtons />
+        <LoadButtons setItems={setItems} />
         <Totals />
 
         <SplitResults />
