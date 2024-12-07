c816e83b300bc40259aeb81e69281f93b6dafcb8
Jimmy Pan <jimmypan13570@gmail.com>
2024-12-06 17:42:39 -0800

    implement loading buttons

--- src/components/LoadButtons.tsx
+++ src/components/LoadButtons.tsx
@@ -1,20 +1,59 @@
+import { createSignal, onCleanup, onMount, Setter, Show } from "solid-js";
+import { getExpandedUrl } from "../utils/requests";
+import { Item } from "../utils/types";
+import { parseURL } from "../utils/utils";
 import { Button } from "./ui/button";
 
-export default function LoadButtons() {
+export default function LoadButtons(props: { setItems: Setter<Item[]> }) {
   const buttonClass =
     "border bg-white text-black hover:bg-gray-200 h-auto px-2 py-1";
 
+  const [online, setOnline] = createSignal(window.navigator.onLine);
+
+  const updateStatus = () => {
+    setOnline(window.navigator.onLine);
+  };
+
+  onMount(() => {
+    window.addEventListener("online", updateStatus);
+    window.addEventListener("offline", updateStatus);
+  });
+
+  onCleanup(() => {
+    window.removeEventListener("online", updateStatus);
+    window.removeEventListener("offline", updateStatus);
+  });
+
   return (
     <>
-      <div class="flex gap-x-1">
-        <input id="file" class="hidden" type="file" accept="image/*" />
-        <Button class={buttonClass} id="custom_file">
-          Upload Receipt
-        </Button>
-        <Button class={buttonClass} id="load_url">
-          Load From URL
-        </Button>
-      </div>
+      <Show when={online()}>
+        <div class="flex gap-x-1">
+          <input id="file" class="hidden" type="file" accept="image/*" />
+          <Button
+            class={buttonClass}
+            onClick={() => {
+              document.getElementById("file")?.click();
+            }}
+          >
+            Upload Receipt
+          </Button>
+          <Button
+            class={buttonClass}
+            onClick={() => {
+              const url = prompt("Enter URL") || "";
+              if (url) {
+                if (url.includes("short.jpkit.us")) {
+                  props.setItems(parseURL(getExpandedUrl(url)));
+                } else {
+                  props.setItems(parseURL(url));
+                }
+              }
+            }}
+          >
+            Load From URL
+          </Button>
+        </div>
+      </Show>
     </>
   );
 }
