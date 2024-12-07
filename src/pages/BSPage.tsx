af43429b746528a06a746715b7b744b6188f9fe7
Jimmy Pan <jimmypan13570@gmail.com>
2024-12-06 16:21:06 -0800

    fix all component refreshing

--- src/pages/BSPage.tsx
+++ src/pages/BSPage.tsx
@@ -34,7 +34,6 @@ export default function BSPage() {
               price: "",
               participants: [],
             });
-            setItems([]);
             setItems(records);
           }}
         />
