eb4453a9b63551be8e5eb99b308c7268c1acf626
Jimmy Pan <jimmypan13570@gmail.com>
2025-01-24 23:42:19 -0800

    refactor seperate components

--- src/components/Totals.tsx
+++ src/components/Totals.tsx
@@ -1,9 +1,7 @@
-import { evaluate } from "mathjs";
-import { Show, createMemo, createSignal } from "solid-js";
+import { createMemo, createSignal } from "solid-js";
 import { Item, Mode } from "../utils/types";
 import { priceToFloat } from "../utils/utils";
-import { Button } from "./ui/button";
-import { Input } from "./ui/input";
+import InputField from "./InputField";
 
 export default function Totals(props: { items: Item[] }) {
   const [fee, setFee] = createSignal<string>("");
@@ -72,84 +70,6 @@ export default function Totals(props: { items: Item[] }) {
         />
         <h2>Total: ${total().toFixed(2) || "0.00"}</h2>
       </div>
-    </>
-  );
-}
-
-function InputField(props: {
-  label?: string;
-  value?: string;
-  mode?: string;
-  subtotal?: string | number;
-  onChange?: (value: string, mode: string) => void;
-}) {
-  const inputField =
-    "w-auto h-auto inline p-0 bg-white text-black text-center focus-visible:ring-0 focus-visible:ring-offset-0 rounded-none";
-  const buttonClass =
-    "border border-gray-200 bg-white text-black hover:bg-gray-100 h-auto px-2 py-0 rounded-none";
-
-  const [mode, setMode] = createSignal<string>(props.mode || Mode.DOLLAR);
-
-  const displayValue = createMemo(() => {
-    const value = parseFloat(props.value || "0");
-    const subtotal = parseFloat(String(props.subtotal) || "0");
-
-    if (mode() === "$") {
-      return parseFloat(((value / subtotal) * 100).toFixed(2));
-    } else {
-      return ((value * subtotal) / 100).toFixed(2);
-    }
-  });
-
-  return (
-    <>
-      <h3>
-        {props.label}:{" "}
-        <Show when={mode() === Mode.DOLLAR}>
-          <Button
-            class={`${buttonClass} border-r-0 rounded-l-md`}
-            onclick={() => {
-              setMode(Mode.PERCENTAGE);
-              props.onChange?.(props.value || "", mode());
-            }}
-          >
-            $
-          </Button>
-        </Show>
-        <Input
-          class={inputField}
-          classList={{
-            "rounded-r-md": mode() === Mode.DOLLAR,
-            "rounded-l-md": mode() === Mode.PERCENTAGE,
-          }}
-          onChange={(e) => {
-            const value = e.target.value.replace(/[^0-9.\/*\-+()%]/g, "");
-            props.onChange?.("", mode());
-            try {
-              props.onChange?.(evaluate(value).toFixed(2), mode());
-            } catch {
-              props.onChange?.(value, mode());
-            }
-          }}
-          value={props.value}
-        />
-        <Show when={mode() === Mode.PERCENTAGE}>
-          <Button
-            class={`${buttonClass} border-l-0 rounded-r-md`}
-            onclick={() => {
-              setMode(Mode.DOLLAR);
-              props.onChange?.(props.value || "", mode());
-            }}
-          >
-            %
-          </Button>
-        </Show>
-      </h3>
-      <h3>
-        {props.label}: {mode() === Mode.PERCENTAGE ? "$" : ""}
-        {displayValue() || (mode() === Mode.DOLLAR ? "0" : "0.00")}
-        {mode() === Mode.DOLLAR ? "%" : ""}
-      </h3>
     </>
   );
 }
